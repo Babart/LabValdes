a883f9d6d005e7dcdca9225fa713286fb4d23c92
Babart <gaspar.av88@gmail.com>
2022-07-06 23:15:20 -0300

    agregado de funcion correccion

--- js/calculo.js
+++ js/calculo.js
@@ -14,23 +14,39 @@ let trigli = prompt("Ingrese su valor de Triglicéridos")
 
 let Triglicéridos = 0; 
 
-
 let colLDL = prompt("Ingrese su valor de Colesterol LDL")
 
 let colesterolLDL = 0;
 
-let indice = 0;
+function indice (colesterolTotal, colesterolHDL){
 
-colesterolTotal/colesterolHDL;
+    if  (!colesterolTotal && !colesterolHDL) {
+        alert("Colesterol total y HDL son valores requeridos")
+    }
 
-if (indice < 4){
+    if (colesterolHDL === 0) {
+        alert ("Coelsterol HDL no puede ser 0")
+    }
 
-    alert ("Su índice es normal");
+    let calculoIndice = colesterolTotal/colesterolHDL;
+
+    if (calculoIndice < 4){
+
+        alert (namePaciente + " Su índice es normal");
+    
+    } else {
+        alert (namePaciente + " Su indice supera el valor esperado, consulte con su médico de cabecera")
+    };
+}
+
+/* if (indice < 4){
+
+    alert (namePaciente + " Su índice es normal");
 
 } else {
-    alert ("Su indice supera el valor esperado, consulte con su médico de cabecera")
+    alert (namePaciente + " Su indice supera el valor esperado, consulte con su médico de cabecera")
 };
-
+ */
 
 
 
