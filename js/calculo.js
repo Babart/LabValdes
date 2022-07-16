b4b8362db3297bfd8def9908c609c1903e114ac7
Babart <gaspar.av88@gmail.com>
2022-07-16 17:31:05 -0300

    primera modificacion para DOM

--- js/calculo.js
+++ js/calculo.js
@@ -1,3 +1,8 @@
+/* 
+
+
+let calculoResultado = document.getElementById("calculoResultado");
+
 
 class Paciente {
     constructor (id, colTotal, colHDL, indice) {
@@ -10,6 +15,7 @@ class Paciente {
 }
 
 const historicoAnalisis = [];
+
 
 function entrada (){
     let namePaciente = prompt("Buen día, Ingrese su nombre");
@@ -54,6 +60,84 @@ function validaciones(colTotal, colHDL) {
 
     if (colHDL === 0) {
         alert ("Colesterol HDL no puede ser 0")
+        return false
+    };
+    return true
+}
+
+
+entrada();
+
+
+function historico(historicoAnalisis) {
+    for(let i = 0; i < historicoAnalisis.length; i++) {
+        const resultado = indice(historicoAnalisis[i].colTotal, historicoAnalisis[i].colHdl);
+        historicoAnalisis[i].indice = resultado;
+    }
+}
+ */
+
+
+
+let calculoResultado = document.getElementById("calculoResultado");
+
+
+class Paciente {
+    constructor (id, colTotal, colHDL, indice) {
+        this.id = id;
+        this.colTotal = colTotal;
+        this.colHDL = colHDL;
+        this.indice = indice; 
+    }
+    
+}
+
+const historicoAnalisis = [];
+
+
+function entrada (){
+    let namePaciente = prompt("Buen día, Ingrese su nombre");
+    let numeroEstudio = parseInt(prompt("Ingrese la cantidad de estudios a cargar"))
+    console.log(numeroEstudio)
+    for(let i = 0; i < numeroEstudio; i++) {
+        indice(i)
+    }
+    calculoResultado.innerHTML = ("Los resultados de " + namePaciente + " son " + JSON.stringify(historicoAnalisis, null, 2))
+    
+}
+
+function indice (i){
+    
+    let colTotal = +prompt("Ingrese su valor de Colesterol Total");
+    let colHDL =  +prompt("Ingrese su valor de Colesterol HDL");
+
+    const isValid = validaciones(colTotal, colHDL)
+    if (!isValid) {
+        return null;
+    }
+
+    let calculoIndice = colTotal/colHDL;
+
+
+    if (calculoIndice < 4){
+
+        calculoResultado.innerHTML = (" Su índice es normal");
+    
+    } else {
+        calculoResultado.innerHTML = (" Su indice supera el valor esperado, consulte con su médico de cabecera")
+    };
+    historicoAnalisis.push(new Paciente(i, colTotal, colHDL, calculoIndice))
+    return null
+}
+
+function validaciones(colTotal, colHDL) {
+    if  (!colTotal && !colHDL) {
+        calculoResultado.innerHTML = ("Colesterol total y HDL son valores requeridos")
+        return false
+    };
+
+    if (colHDL === 0) {
+        calculoResultado.innerHTML = ("Colesterol HDL no puede ser 0")
         return false
     };
     return true
